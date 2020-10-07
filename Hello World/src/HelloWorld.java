2cead9bd10c5b169352f114b362d98b2f836fd80
Ziaul572 <60806758+Ziaul572@users.noreply.github.com>
2020-10-07 23:13:37 +0600

    Add files via upload

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,8 @@
+	//Printing program.
+public class HelloWorld {
+	//start of function
+		public static void main(String [] args) {
+			System.out.print("Welcome.\n");
+			System.out.println("HelloWorld");
+		} //end of function
+} // end of class HelloWorld.
